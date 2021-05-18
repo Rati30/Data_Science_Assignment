23ed5b6ec546c932ed62fc65f1a8a2ead6dae2ed
Rati Priya <82707008+Rati30@users.noreply.github.com>
2021-05-18 13:41:47 +0530

    Add files via upload

---
+++ Python_Assignment1.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a="Hello"            ### Question 1
+print(a)
+
+
+# In[2]:
+
+
+b="Hello World!"    ### Question 2
+print(b)
+
+
+# In[3]:
+
+
+b="World!"         ### Question 2
+print(b)
+
+
+# In[8]:
+
+
+c="Jai"              ### Question 3
+print("Hello", c, "Let's start learning Python")
+
+
+# In[11]:
+
+
+#This program is to print a sentence having double quotation " " in output.
+print('''Dr. A.P.J.Abdul Kalam once said,\n"DREAMS \nis not what you see in sleep \nis the thing which doesn't let you sleep" ''')    ### Question 4
+
+
+# In[18]:
+
+
+famous_person="Dr. A. P. J. Abdul Kalam"   ### Question 5
+message=famous_person, '''once said,\n"DREAMS is not what you see in sleep is the thing which doesn't let you sleep"'''
+print(message)
+
+
+# In[21]:
+
+
+#Rati Priya Date-18/05/2021 
+#This program is to print a single value ans i.e, 8 through various mathamatical calculation (addition, substraction, multiplication, division) 
+print(2+6)          ### Question 6
+print(10-2)
+print(4*2)
+print(int(16/2))
+
+
+# In[23]:
+
+
+c=1                ### Question 7
+message='Favourate no is :',c
+print(message)
+
+
+# In[27]:
+
+
+names=['Neha','Riya','jiya','Ravi','Jai']      ### Question 9
+names[0]
+
+
+# In[28]:
+
+
+names[1]
+
+
+# In[29]:
+
+
+names[2]
+
+
+# In[32]:
+
+
+names[3:5]
+
+
+# In[40]:
+
+
+message={'Neha':'hello', 'Riya':'hello', 'jiya':'hello', 'Ravi':'hello', 'Jai':'hello'}  ### Question 10
+message['Neha']
+
+
+# 
+
+# In[44]:
+
+
+bike= ['Royal enfield','Bajaj Avenger','ktm']            ### Question 11
+statement={bike[0]:"I would like go for Ladakh trip with Royal Enfield",bike[1]:"It is very comfortable to drive for long time with Bajaj Avanger",bike[2]:"I would like to go for bike race with KTM"}
+
+
+# In[45]:
+
+
+statement
+
+
+# In[ ]:
+
+
+
+
